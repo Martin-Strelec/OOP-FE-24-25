93e5325111930428f026e70971f13a45ba018bb2
Martin Strelec <m.strelec2021@gmail.com>
2025-01-08 12:32:48 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OOP_FE_24_25
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
