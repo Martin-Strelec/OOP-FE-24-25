b05b15f744d1171ceea4cd87ea4939ae2fd169f9
Martin Strelec <m.strelec2021@gmail.com>
2025-01-08 13:35:34 +0000

    Objects are created in the Window_Loaded event

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,10 +19,24 @@ namespace OOP_FE_24_25
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Event> events = new List<Event>();
+        List<Ticket> tickets = new List<Ticket>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Event ev1 = new Event("Oasis Croke Park", new DateTime(2025,06,20), Event.TypeOfEvent.Music);
+            Event ev2 = new Event("Oasis Croke Park", new DateTime(2025, 08, 20), Event.TypeOfEvent.Music);
+
+            Ticket tic1 = new Ticket("Early Bird", 100m, 100);
+            Ticket tic2 = new Ticket("Platinum", 150m, 100);
+
+            VIPTicket vtic1 = new VIPTicket("Ticket and Hotel Package", 150m, 100, "4* Hotel", 100m);
+            VIPTicket vtic2 = new VIPTicket("Weekend Ticket", 200m, 100, "with camping", 100m);
+        }
     }
 }
