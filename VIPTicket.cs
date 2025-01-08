aa0be4dc60da0349f82788dac0f610df28a776f1
Martin Strelec <m.strelec2021@gmail.com>
2025-01-08 13:40:37 +0000

    Added ToString method

--- VIPTicket.cs
+++ VIPTicket.cs
@@ -9,14 +9,20 @@ namespace OOP_FE_24_25
     internal class VIPTicket : Ticket
     {
         #region Properties
-        public string AdditionalCosts { get; set; }
+        public decimal AdditionalCosts { get; set; }
         public string AdditionalExtras { get; set; }
         #endregion
 
-        public VIPTicket(string name, decimal price, int availableTickets, string addtitionalExtras, string additionalCosts) : base(name, price, availableTickets) 
+        public VIPTicket(string name, decimal price, int availableTickets, string addtitionalExtras, decimal additionalCosts) : base(name, price, availableTickets) 
         {
             this.AdditionalExtras = addtitionalExtras;
             this.AdditionalCosts = additionalCosts;
+            this.Price = price + additionalCosts;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Price:c} ({AdditionalExtras}) [AVAILABLE - {AvailableTickets}]";
         }
     }
 }
