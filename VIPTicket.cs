cc86c55e6ed766766d19661cfa48b737396dce50
Martin Strelec <m.strelec2021@gmail.com>
2025-01-08 13:15:51 +0000

    Added VIPTicket class

---
+++ VIPTicket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_FE_24_25
+{
+    internal class VIPTicket : Ticket
+    {
+        #region Properties
+        public string AdditionalCosts { get; set; }
+        public string AdditionalExtras { get; set; }
+        #endregion
+
+        public VIPTicket(string name, decimal price, int availableTickets) : base(name, price, availableTickets) { }
+    }
+}
