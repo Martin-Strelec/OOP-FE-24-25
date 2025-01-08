8ae1665c94879cd549ae686eb2ab7de210bfed7a
Martin Strelec <m.strelec2021@gmail.com>
2025-01-08 13:11:11 +0000

    Added ticket class

---
+++ Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_FE_24_25
+{
+    internal class Ticket
+    {
+        #region Properties
+        public string Name { get; set; } 
+        public decimal Price { get; set; }
+        public int AvailableTickets { get; set; }
+        #endregion
+
+        public Ticket (string name, decimal price, int availableTickets)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.AvailableTickets = availableTickets;
+        }
+        public Ticket() { }
+    }
+}
